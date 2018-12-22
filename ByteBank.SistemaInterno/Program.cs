65cd323454d13bc58d8d1a6033a25cc2818346a1
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-22 19:35:35 -0200

    Usando nossa DLL gerada por outro projeto do curso

---
+++ ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,15 @@
+﻿using ByteBank.Modelos;
+using System;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(00021, 7687150);
+
+            Console.ReadLine();
+        }
+    }
+}
